0ea51d86a5823d1995ed8ea6847dfa535154d555
asliddin4949 <asliddin4949@inbox.ru>
2023-01-12 12:46:56 +0500

    console

--- src/main/java/com/pdp/bookbus/console/impl/UserConsole.java
+++ src/main/java/com/pdp/bookbus/console/impl/UserConsole.java
@@ -8,8 +8,28 @@ import java.util.Scanner;
 public class UserConsole implements IConsole {
     @Override
     public void openConsole(User user) {
-        while (true){
+        while (true) {
             Scanner scanner = new Scanner(System.in);
+            System.out.println("'1' - show travels  '2' - show buses  '0' - exit" +
+                    "\n'3' - buy ticket  '4' - ticket history  '5' - balance  '6' - download ticket");
+            int command = scanner.nextInt();
+            if (command == 1) {
+
+            } else if (command == 2) {
+
+            } else if (command == 3) {
+
+            } else if (command == 4) {
+
+            } else if (command == 5) {
+
+            } else if (command == 6) {
+
+            } else if (command == 0) {
+                break;
+            } else {
+                System.out.println("Wrong command");
+            }
         }
     }
 
